b2a065c670e5c79e3ac89ec1adab625cf92f413d
Tortoe <50211001+Tortoe@users.noreply.github.com>
2019-05-12 20:40:40 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+client.login(process.env.TOKEN);
+
+
+fs.readdir('./Commandes/', (error, f) => {
+    if (error) { return console.error(error); }
+        let commandes = f.filter(f => f.split('.').pop() === 'js');
+        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
+
+        commandes.forEach((f) => {
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} commande chargée !`);
+            client.commands.set(commande.help.name, commande);
+        });
+});
+
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
+
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
+});
+
+client.on('message', async message => {
+
+    if(message.content === "subscribe")
+    {
+        message.channel.send("to pewdiepie")
+        console.log("pewdiepie")
+    }
+
+});
